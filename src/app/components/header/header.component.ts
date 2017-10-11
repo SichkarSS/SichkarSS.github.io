588dd55471e435a5e59e328980af3251d7de054a
stepan.sichkar <stepan.sichkar@arcadia.spb.ru>
2017-10-11 14:13:58 +0300

    fix header width and animation of the ball

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,28 +6,37 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-    public isMouseOver = false;
-    public isTransitionEnd = false;
+    @ViewChild('headerContainer') headerContainer: ElementRef;
+
+    public ballShouldBeDown = false;
+    public ballShouldBeLeftOut = false;
+    public startFalling = false;
+    public isAnimationShouldStart = false;
     public window = window;
+    public leftOutOfScreen: number;
+    public ICON_SIZE = 30;
 
     constructor() {
     }
 
     ngOnInit() {
+        const mayBeMargin = (window.innerWidth - this.headerContainer.nativeElement.clientWidth) / 2;
+        this.leftOutOfScreen = (mayBeMargin > 0 ? mayBeMargin : 0) + this.ICON_SIZE;
     }
 
     moveTheBall() {
-        this.isMouseOver = true;
-        this.isTransitionEnd = false;
+        this.ballShouldBeDown = true;
+        this.isAnimationShouldStart = true;
     }
 
     returnTheBall() {
         setTimeout(() => {
-            this.isMouseOver = false;
-        }, 500);
+            this.ballShouldBeDown = false;
+            this.isAnimationShouldStart = false;
+        }, 1000);
     }
 
     onTransitionEnd() {
-        this.isTransitionEnd = true;
+        this.isAnimationShouldStart = false;
     }
 }
